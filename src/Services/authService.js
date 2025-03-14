58d0e300da4b4acdd34e67b5e3f4ea8a8e4c4473
Adrilo31 <adrienlandras31@gmail.com>
2025-03-14 14:21:32 +0100

    GestionStocks

--- src/Services/authService.js
+++ src/Services/authService.js
@@ -1,11 +1,52 @@
-export const getToken = () => localStorage.getItem("token");
+import { jwtDecode } from "jwt-decode";
+import axios from "axios";
 
-export const isAuthenticated = () => {
-    const token = getToken();
-    return !!token; // Retourne true si un token est présent
+export const loginUser = async (email, password) => {
+    const response = await axios.post("http://mapmarketapi.test/api/login", {
+        email,
+        password,
+    });
+    const token = response.data.token;
+    localStorage.setItem("token", token);
+    console.log("Token reçu et stocké :", token); // Log du token reçu
+    return response.data;
 };
 
-export const logout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login"; // Redirige vers la page de connexion
+export const logoutUser = async (navigate) => {
+    try {
+        const token = localStorage.getItem("token");
+        console.log("Token utilisé pour la déconnexion :", token); // Log du token utilisé
+        await axios.post(
+            "http://mapmarketapi.test/api/logout?Accept=application/json",
+            {},
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        localStorage.removeItem("token");
+        navigate("/login");
+    } catch (error) {
+        console.error("Erreur lors de la déconnexion :", error);
+    }
+};
+
+export const getUserInfo = () => {
+    const token = localStorage.getItem("token");
+    console.log("Token récupéré pour décodage :", token); // Log du token récupéré
+    if (token) {
+        try {
+            const decodedToken = jwtDecode(token);
+            console.log("Token décodé :", decodedToken); // Log du token décodé
+            return {
+                nom: decodedToken.nom,
+                prenom: decodedToken.prenom,
+            };
+        } catch (error) {
+            console.error("Erreur lors du décodage du token :", error);
+            return null;
+        }
+    }
+    return null;
 };
